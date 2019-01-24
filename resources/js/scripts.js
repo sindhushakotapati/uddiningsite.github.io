a1c979ba1122bfdd2081b1d9c9a3177601a2ef29
Sindhusha Kotapati <sindhusha.kotapati@gmail.com>
2019-01-24 00:14:10 -0500

    Frontend for the university dining services
    The site currently contains the front end components. The webpage is responsive all the components change their size and positioning with changing screen sizes.

---
+++ resources/js/scripts.js
@@ -0,0 +1,23 @@
+  /* eslint-env jquery */
+  /* global document */
+
+    $(document).ready(function() {
+        
+    $('.js--section-features').waypoint(function(direction) {
+       if (direction == "down") {
+           $('nav').addClass('sticky');
+       }
+        else{
+          $('nav').removeClass('sticky');
+       }
+    });
+  
+   /*
+   var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+*/
+    
+});
